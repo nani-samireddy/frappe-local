0e884fb34981f5f48f1a85855b9598d8d6eb33b9
nani-samireddy <nanisamireddy05@gmail.com>
2025-02-16 00:52:03 +0530

    enhancement: add tauri shell plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -29,6 +29,7 @@ fn init_bench(name: String) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, init_bench])
         .run(tauri::generate_context!())
