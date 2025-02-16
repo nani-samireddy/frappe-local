68c96d6c48f6e344986cc3cec1fd49e510d743a9
nani-samireddy <nanisamireddy05@gmail.com>
2025-02-17 00:31:28 +0530

    chore: convert shell script into tauri ts script

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { useRef } from "react";
-import { invoke } from "@tauri-apps/api/core";
+import { useRef, useState } from "react";
+// import { invoke } from "@tauri-apps/api/core";
+import { setupBench } from "./actions/setup-bench";
 import "./App.css";
 
 function App() {
   const benchNameRef = useRef<HTMLInputElement>(null);
+  const [message, setMessage] = useState<string | null>(null);
   const handleCreateBench = async () => {
     if (benchNameRef.current) {
       const benchName = benchNameRef.current.value;
       try {
-        const res = await invoke("init_bench", { name: benchName });
-        console.log(res);
+        // const res = await invoke("init_bench", { name: benchName });
+        // console.log(res);
+        await setupBench(benchName, setMessage);
       } catch (error) {
         console.error(error);
       }
@@ -20,6 +23,14 @@ function App() {
      <h1>hehehhehe</h1>
      <input type="text" ref={benchNameRef} placeholder="Enter bench name" />
      <button onClick={handleCreateBench}>Create</button>
+      {message && <>
+        <h3>Progress</h3>
+        {
+          message.split("<br/>").map((msg, index) => (
+            <p key={index}>{msg}</p>
+          ))
+        }
+      </>}
     </main>
   );
 }
---
+++ src/actions/setup-bench.ts
@@ -0,0 +1,195 @@
+import { Command } from "@tauri-apps/plugin-shell";
+import { homeDir } from "@tauri-apps/api/path";
+import {
+  mkdir,
+  BaseDirectory,
+  create,
+  readTextFile,
+  writeTextFile,
+  copyFile,
+} from "@tauri-apps/plugin-fs";
+// import { message } from "@tauri-apps/plugin-dialog";
+
+async function runCommand(cmd: string, args: string[], step: string) {
+  try {
+    const result = await Command.create("exec-sh", [
+      "-c",
+      `${cmd} ${args.join(" ")}`,
+    ]).execute();
+
+    console.log(`${step} completed with output:`, result.stdout);
+
+    if (result.code !== 0) {
+      console.error(`Error in step: ${step}\n${result.stderr}`);
+    }
+  } catch (error) {
+    console.error(`Failed to execute ${step}:`, error);
+  }
+}
+
+// Function to check if a port is available
+async function getUnusedPorts(): Promise<number[]> {
+  const result = await Command.create("exec-sh", [
+    "-c",
+    "bash -c \"comm -23 <(seq 1024 65535 | sort) <(lsof -i -P -n | awk 'NR>1 {print $9}' | awk -F: '{print $NF}' | sort -u)\"",
+  ]).execute();
+  return result.stdout
+    .split("\n")
+    .map(Number)
+    .filter((n) => !isNaN(n));
+}
+
+async function setupBench(projectName: string, messageCallback: any) {
+  const home = await homeDir(); // Get the home directory from Tauri API
+  const baseDir = `${home}/benches`; // Correctly build the path
+  const projectDir = `${baseDir}/${projectName}`;
+
+  const sourceDockerCompose = "template-files/docker-compose.yaml";
+
+  // Get available ports
+  const availablePorts = await getUnusedPorts();
+  if (availablePorts.length < 13) {
+    console.error("Not enough available ports");
+    return;
+  }
+
+  const [frappePortStart, frappeAltPortStart, dbViewerPort] = [
+    availablePorts[0],
+    availablePorts[6],
+    availablePorts[12],
+  ];
+  const frappePortEnd = frappePortStart + 5;
+  const frappeAltPortEnd = frappeAltPortStart + 5;
+
+  // Create source directory
+  await mkdir(`${projectDir}/source`, { recursive: true });
+  // Append the message to the callback
+  messageCallback(
+    (message: string) =>
+      message + "<br/>created source directory at " + projectDir + "/source✅"
+  );
+  // Create logs directory
+  await mkdir(`${projectDir}/logs`, { recursive: true });
+  messageCallback(
+    (message: string) =>
+      message + "<br/>created logs directory at " + projectDir + "/logs✅"
+  );
+
+  // Create bench.log file
+  await create(`${projectDir}/logs/bench.log`);
+  messageCallback(
+    (message: string) =>
+      message +
+      "<br/>created bench.log file at " +
+      projectDir +
+      "/logs/bench.log✅"
+  );
+
+  // Read the contents of the docker-compose file.
+  const dockerCompose = await readTextFile(sourceDockerCompose, {
+    baseDir: BaseDirectory.Resource,
+  });
+  // Update the contents of the docker-compose file.
+  const updatedDockerCompose = dockerCompose.replace(
+    /project_name/g,
+    projectName
+  );
+
+  // Create the updated docker-compose file.
+  await writeTextFile(
+    `${projectDir}/docker-compose.yaml`,
+    updatedDockerCompose
+  );
+  messageCallback(
+    (message: string) =>
+      message +
+      "<br/>created updated docker-compose file at " +
+      projectDir +
+      `/docker-compose.yaml✅`
+  );
+
+  // Copy the installer script to the source directory
+  await copyFile(
+    "template-files/installer.py",
+    `${projectDir}/source/installer.py`,
+    { fromPathBaseDir: BaseDirectory.Resource }
+  );
+  messageCallback(
+    (message: string) =>
+      message +
+      "<br/>copied installer script to source directory at " +
+      projectDir +
+      "/source/installer.py✅"
+  );
+
+  // Update the installer script file permissions
+  await runCommand(
+    "chmod",
+    ["+x", `${projectDir}/source/installer.py`],
+    "Making installer script executable"
+  );
+
+  // // Make installer script executable
+  // await runCommand(
+  //   "chmod",
+  //   ["+x", updatedInstallerScript],
+  //   "Making installer script executable"
+  // );
+
+  // // Start Docker Compose
+  // process.env.FRAPPE_PORT_START     = frappePortStart.toString();
+  // process.env.FRAPPE_PORT_END       = frappePortEnd.toString();
+  // process.env.FRAPPE_ALT_PORT_START = frappeAltPortStart.toString();
+  // process.env.FRAPPE_ALT_PORT_END   = frappeAltPortEnd.toString();
+  // process.env.DB_VIEWER_PORT        = dbViewerPort.toString();
+
+  const envVars = {
+    FRAPPE_PORT_START: frappePortStart.toString(),
+    FRAPPE_PORT_END: frappePortEnd.toString(),
+    FRAPPE_ALT_PORT_START: frappeAltPortStart.toString(),
+    FRAPPE_ALT_PORT_END: frappeAltPortEnd.toString(),
+    DB_VIEWER_PORT: dbViewerPort.toString(),
+  };
+
+  const envString = Object.entries(envVars)
+    .map(([key, value]) => `${key}=${value}`)
+    .join(" ");
+
+  await runCommand(
+    `cd ${projectDir} && ${envString} docker-compose`,
+    ["-f", `docker-compose.yaml`, "up", "-d"],
+    "Starting Docker Compose"
+  );
+  messageCallback(
+    (message: string) => message + "<br/>Started Docker Compose✅"
+  );
+
+  // Wait for container to start
+  const frappeContainerName = `${projectName}-frappe-1`.toLowerCase();
+
+  let isRunning = false;
+  while (!isRunning) {
+    const inspectResult = await Command.create("exec-sh", [
+      "-c",
+      `docker inspect -f '{{.State.Running}}' ${frappeContainerName}`,
+    ]).execute();
+    isRunning = inspectResult.stdout.trim() === "true";
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+
+  // Get container ID
+  const containerIdResult = await Command.create("exec-sh", [
+    "-c",
+    `docker ps -aqf "name=${frappeContainerName}"`,
+  ]).execute();
+  const containerId = containerIdResult.stdout.trim();
+
+  // Run installer script inside the container
+  await runCommand(
+    "docker",
+    ["exec", "-i", containerId, "./installer.py"],
+    "Running installer script"
+  );
+}
+
+export { setupBench };
